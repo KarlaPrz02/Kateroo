0c7c7b44e8c7d8ef58744e9e7547739cf206511a
Karla <imcarla2002@gmail.com>
2024-08-30 17:41:34 +0200

    Added pat sounds (the slime blocks shit)

--- src/main/java/me/kat/kateroo/client/config/KaterooModMenu.java
+++ src/main/java/me/kat/kateroo/client/config/KaterooModMenu.java
@@ -9,24 +9,29 @@ import me.shedaniel.clothconfig2.api.ConfigCategory;
 public class KaterooModMenu implements ModMenuApi {
 
     private static final String RENDERING_CATEGORY_TITLE = "category.kateroo.rendering";
+    private static final String PLAYER_CATEGORY_TITLE = "category.kateroo.player";
+
 
     public static final BooleanOption noLavaFog                        = new BooleanOption("noLavaFog", false);
     public static final BooleanOption disableBlockBreakingParticles    = new BooleanOption("disableBlockBreakingParticles", false);
     public static final BooleanOption disableCameraMovementWhenDamage  = new BooleanOption("disableCameraMovementWhenDamage", false);
     public static final BooleanOption disableFireOnCam                 = new BooleanOption("disableFireOnCam", false);
     public static final BooleanOption miningRewardDrops                = new BooleanOption("miningRewardDrops", false);
+    public static final BooleanOption disableSlimeBlockPlayerMovement  = new BooleanOption("disableSlimeBlockPlayerMovement", false);
 
     @Override
     public ConfigScreenFactory<?> getModConfigScreenFactory() {
         return parent -> {
             ConfigBuilder builder = ConfigBuilder.create();
             ConfigCategory renderingCategory = builder.getOrCreateCategory(RENDERING_CATEGORY_TITLE);
+            ConfigCategory playerCategory = builder.getOrCreateCategory(PLAYER_CATEGORY_TITLE);
 
             renderingCategory.addEntry(noLavaFog.asConfigEntry(builder));
             renderingCategory.addEntry(disableBlockBreakingParticles.asConfigEntry(builder));
             renderingCategory.addEntry(disableCameraMovementWhenDamage.asConfigEntry(builder));
             renderingCategory.addEntry(disableFireOnCam.asConfigEntry(builder));
             renderingCategory.addEntry(miningRewardDrops.asConfigEntry(builder));
+            playerCategory.addEntry(disableSlimeBlockPlayerMovement.asConfigEntry(builder));
 
             return builder.build();
         };
---
+++ src/main/java/me/kat/kateroo/client/mcwrapper/mixin/SlimeBlockMixin.java
@@ -0,0 +1,39 @@
+package me.kat.kateroo.client.mcwrapper.mixin;
+
+import me.kat.kateroo.client.config.KaterooModMenu;
+import net.minecraft.block.SlimeBlock;
+import net.minecraft.entity.Entity;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.Redirect;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(SlimeBlock.class)
+public class SlimeBlockMixin {
+    @Redirect(
+            method = "onSteppedOn",
+            at = @At(value = "INVOKE", target = "Lnet/minecraft/entity/Entity;bypassesSteppingEffects()Z")
+    )
+    private boolean handleSlimeConfig(Entity instance){
+        if (KaterooModMenu.disableSlimeBlockPlayerMovement.getValue()) {
+            return true;
+        } else {
+            return instance.bypassesSteppingEffects();
+        }
+    }
+
+    @Inject(
+            method = "method_21847",
+            at = @At("HEAD"),
+            cancellable = true
+
+    )
+    private void onMethod21847(CallbackInfo ci) {
+        if (KaterooModMenu.disableSlimeBlockPlayerMovement.getValue()) {
+            ci.cancel();
+        }
+    }
+}
+
+
