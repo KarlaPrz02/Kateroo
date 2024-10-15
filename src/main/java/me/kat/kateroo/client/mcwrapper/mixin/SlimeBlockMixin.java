ba25710868fa4f3d16cb065c8ae8c895a66db3fe
Karla <imcarla2002@gmail.com>
2024-10-16 01:09:05 +0200

    Delete MixinSlimeBlock.java and merge to SlimeBlockMixin.java

--- src/main/java/me/kat/kateroo/client/mcwrapper/mixin/SlimeBlockMixin.java
+++ src/main/java/me/kat/kateroo/client/mcwrapper/mixin/SlimeBlockMixin.java
@@ -2,8 +2,12 @@ package me.kat.kateroo.client.mcwrapper.mixin;
 
 import me.kat.kateroo.client.config.KaterooModMenu;
 import net.minecraft.block.SlimeBlock;
+import net.minecraft.block.TransparentBlock;
 import net.minecraft.entity.Entity;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.Vec3d;
+import net.minecraft.world.World;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -11,7 +15,11 @@ import org.spongepowered.asm.mixin.injection.Redirect;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(SlimeBlock.class)
-public class SlimeBlockMixin {
+public abstract class SlimeBlockMixin extends TransparentBlock {
+    protected SlimeBlockMixin(Settings settings) {
+        super(settings);
+    }
+
     @Redirect(
             method = "onSteppedOn",
             at = @At(value = "INVOKE", target = "Lnet/minecraft/entity/Entity;bypassesSteppingEffects()Z")
@@ -35,6 +43,17 @@ public class SlimeBlockMixin {
             ci.cancel();
         }
     }
+
+    @Inject(method = "onSteppedOn", at =@At("HEAD"), cancellable = true)
+    private void onEntityWalkOnSlime(World worldIn, BlockPos pos, Entity entityIn, CallbackInfo ci)
+    {
+        if (KaterooModMenu.disableSlimeBlockPlayerMovement.getDefaultValue()  && entityIn instanceof PlayerEntity)
+        {
+            super.onSteppedOn(worldIn, pos, entityIn);
+            ci.cancel();
+        }
+    }
+
 }
 
 
